34ac2973f79d09c156f526f1aefbdb634e96a230
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-10-28 23:00:23 +0200

    PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") *speed* Time.deltaTime; 
+        var x = Mathf.Clamp(transform.position.x + deltaX, -5, 5);
+        
+
+        var deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        var y = Mathf.Clamp(transform.position.y + deltaY, -9, 0);
+        transform.position = new Vector2(x, y);
+
+
+    }
+}
